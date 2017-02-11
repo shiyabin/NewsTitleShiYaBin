47ac12deb8bdaf9aa0fcee91a5add4c08ff50c83
shiyabin <shiyabin114@126.com>
2017-02-11 16:08:48 +0800

    第三次上传

--- app/src/main/java/com/bwie/newstitleshiyabin/fragment/Fragment_XW.java
+++ app/src/main/java/com/bwie/newstitleshiyabin/fragment/Fragment_XW.java
@@ -1,13 +1,16 @@
 package com.bwie.newstitleshiyabin.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.bwie.newstitleshiyabin.R;
+import com.bwie.newstitleshiyabin.activity.SouSuo;
 
 /**
  * 1. 类的用途
@@ -17,11 +20,28 @@ import com.bwie.newstitleshiyabin.R;
 
 
 public class Fragment_XW extends Fragment {
+
+    private View view;
+    private EditText fragXwEt;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View view=inflater.inflate(R.layout.fragment_xw,null);
+        view = inflater.inflate(R.layout.fragment_xw,null);
+        kongjian();
 
         return view;
+    }
+
+    private void kongjian() {
+        fragXwEt = (EditText) view.findViewById(R.id.frag_xw_et);
+        fragXwEt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it=new Intent(getActivity(), SouSuo.class);
+                startActivity(it);
+            }
+        });
+
     }
 }
